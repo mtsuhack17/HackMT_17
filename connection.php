10cfd810716cd5e52081f923a8dfc513e91394f3
fio2003 <fio2003@gmail.com>
2017-02-04 16:46:04 -0600

    fixed2

--- connection.php
+++ connection.php
@@ -1,18 +1,22 @@
 <?php
-echo " in connection.php ";
-//database connection
-$servername = "ancient.cs.mtsu.edu";
-$username = "hackmt7";
-$password = "hackmt7mysql";
-$tdb = "hackmt7";
-$port = '443';
+funciton get_db_connection($db = false)
+{
+	// echo " in connection.php ";
+	//database connection
+	$servername = "ancient.cs.mtsu.edu";
+	$username = "hackmt7";
+	$password = "hackmt7mysql";
+	$tdb = "hackmt7";
+	$port = '443';
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $tdb);
+	// Create connection
+	$conn = mysqli_connect($servername, $username, $password, $tdb);
 
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+	// Check connection
+	if (!$conn) {
+	    die("Connection failed: " . mysqli_connect_error());
+	}
+	return $conn;
+	// echo "Connected successfully";
 }
-echo "Connected successfully";
  ?>
