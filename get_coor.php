bee0fcb18c59c1a55e5394627266976a7d98f630
jh6w@mtmail.mtsu.edu <jh6w@mtmail.mtsu.edu>
2017-02-05 09:25:06 -0600

    phone stuff works

--- get_coor.php
+++ get_coor.php
@@ -1,14 +1,17 @@
 <?php
 //saves coordinates sent to POST from phone app
-function get_coordinates($dbc){
-  $longitude = $_POST['lng'];
-  $latitude = $_POST['lat'];
-  $user = $_POST['user'];
+include_once "./connection.php";
 
-  $q = "INSERT INTO POSITION SET USER_ID = $user , COORD_LON = $longitude , COORD_LAT = $latitude  ";
+function get_coordinates($db = false){
+  if(!$db)  { $db = get_db_connection();  }
+  $longitude = floatval($_POST['lng']);
+  $latitude = floatval($_POST['lat']);
+  $user = intval($_POST['user']);
 
-  mysqli_query($dbc, $q);
+  $q = "INSERT INTO POSITION SET USER_ID = $user , COORD_LAT = $longitude , COORD_LON = $latitude  ";
+
+  mysqli_query($db, $q);
 }
 
-
+get_coordinates();
  ?>
